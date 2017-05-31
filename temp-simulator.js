eb37bbbd56a7e3904e5e735acbde0490dadbcf9f
Parthapratim Neog <partha@vebbler.com>
2017-05-31 20:32:04 +0530

    added city name list, and using city.

--- temp-simulator.js
+++ temp-simulator.js
@@ -1,5 +1,18 @@
 io = require('socket.io-client');
+if (process.argv.length <= 2) {
+    console.log("Usage: node " + __filename + " <city_id>");
+    process.exit(-1);
+}
+ 
+var param = process.argv[2];
+
 var socket = io('http://localhost:8081');
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('cities-name-list.json', 'utf8'));
+
+city = obj[param];
+console.log(city);
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -10,6 +23,7 @@ function getRandomInt(min, max) {
 socket.on('connect', function(){
     console.log("Connected to server");
     setInterval(function(){
-        socket.emit('temp-update', {'tinsukia': getRandomInt(0,40)});
+        // socket.emit('temp-update', {city: getRandomInt(0,40)});
+        socket.emit('temp-update', {city: city, temp:getRandomInt(0,40)});
     }, 1000);
 });
